ca02093f08be1e7e0da35f70c11a8fb6a0628e35
bjf2015 <bfranzoi@icloud.com>
2016-07-22 13:05:23 -0700

    Input from form

--- main.js
+++ main.js
@@ -12,8 +12,8 @@
 
 
 ******/
-
-
+var addressBook;
+var addressBookPerson;
 
 $(document).ready(function(){
 
@@ -21,12 +21,6 @@ $(document).ready(function(){
 // create a structured object
 
 
-
-//create add button functionality	
-
-$( "#add-button" ).click(function() {
-  alert( "button was pressed" );
-});
 
 
 
@@ -41,14 +35,52 @@ $( "#add-button" ).click(function() {
 //create a contact link that shows contact name and when clicked displays contact information.
 //display new contact information in detail
 
+var addressBook = new Array();
 
 
 
+var addressBookPerson = function(firstName, lastName, phoneNumber, street, city, state) {
+	this.firstName= firstName;
+	this.lastName= lastName;
+	this.phoneNumber= phoneNumber;
+	this.street= street;
+	this.city= city;
+	this.state= state;
+
+};
+
+var Bryan = new addressBookPerson("Bryan", "Franzoi", "312-965-3100","42 West 3rd", "LV", "NV");
+addressBook[0] = new addressBookPerson;
+console.log(addressBook)
+//firstName.name = $('firstName').value;
+// firstName.name = document.getElementById('firstName').val();
+// firstName.name = document.getElementById('firstName').val();
+// firstName.name = document.getElementById('firstName').val();
+// firstName.name = document.getElementById('firstName').val();
+// firstName.name = document.getElementById('firstName').val();
 
 
 
+//addressBookPerson.forEach(function(addressBookPerson) {
+  //console.log(addressBookPerson.name + " named:");
+  // adressBookPerson.lastName.forEach(function(lastName) {
+  //   console.log(lastName.name);
+  // });
+//});
 
 
+
+//create add button functionality	
+
+$( "#add-button" ).click(function(event) {
+	event.preventDefault();
+	//addressBookName.push($('#firstName').val());
+	addressBook.push( new addressBookPerson($('#firstName').val(), $('#lastName').val(), $('#phoneNumber').val(),
+											 $('#street').val(), $('#city').val(), $('#state').val()));
+	//,addressBook.push( 'Ford')
+	console.log(addressBook);
+
+});
 
 
 
