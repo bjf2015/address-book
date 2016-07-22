05b201780b045c84ba8fd5da485b8e37c8a2d3f9
mrmojica <mrmojica510@gmail.com>
2016-07-22 09:48:57 -0700

    added additional sections

--- main.js
+++ main.js
@@ -13,4 +13,48 @@
 
 ******/
 
-///create new section for entered list in html
+
+
+$(document).ready(function(){
+
+
+// create a structured object
+
+
+
+//create add button functionality	
+
+$( "#add-button" ).click(function() {
+  alert( "button was pressed" );
+});
+
+
+
+// create a new object function that inherits structured object
+
+// one user clicks addButton new properties/values get added to the new object
+
+
+// append list item with user named wrapped around a link/button and when clicked shows detailed information.
+	//new list item will be created and added to ul class="contact-list".
+
+//create a contact link that shows contact name and when clicked displays contact information.
+//display new contact information in detail
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
