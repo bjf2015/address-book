7cb75c44db7327cc0a52aefef963019926f97b0e
bjf2015 <bfranzoi@icloud.com>
2016-07-22 09:06:30 -0700

    form built

---
+++ main.js
@@ -0,0 +1,16 @@
+/*******
+
+-allow users to create an store contacts 
+-once users adds contacts should see list of contacts underneath input
+-user should be able to click contact to view their information on side of the form
+
+-each contact should be stored as an object with a number of properties
+-submitting a form should create a new object with whatever properties are submitted
+	-user should not be able to submit a new contact w/o min info (name)
+
+-EXTRA create edit contact option
+
+
+******/
+
+///create new section for entered list in html
